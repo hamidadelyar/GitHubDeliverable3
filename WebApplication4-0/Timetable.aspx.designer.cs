9472b194e36520da2578cadcd5af4f2bff6c730c
hamid adelyar <hamidadelyar@gmail.com>
2015-03-12 17:03:43 +0000

    took out test <p> tag

--- WebApplication4-0/Timetable.aspx.designer.cs
+++ WebApplication4-0/Timetable.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace WebApplication4_0 {
     
     
     public partial class Timetable {
-        
-        /// <summary>
-        /// sessionTest control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl sessionTest;
     }
 }
