a90a9b5fe8b04e099cd9572e36157358d954a211
afreeman123 <freeman.aidan@gmail.com>
2015-03-25 14:55:56 +0000

    Initial View Requests page

---
+++ WebApplication4-0/ViewRequest.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ViewRequest.aspx.cs" Inherits="WebApplication4_0.ViewRequest" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<hgroup class="header">
+    <h1>Your Requests</h1>
+</hgroup>
+<section class ="tableOfRequests">
+    
+<asp:Table ID="Table1" runat="server" GridLines="Both">
+        <asp:TableRow>
+            <asp:TableCell>Semester</asp:TableCell>
+            <asp:TableCell>Day</asp:TableCell>
+            <asp:TableCell>Week(s)</asp:TableCell>
+            <asp:TableCell>Time</asp:TableCell>
+            <asp:TableCell>Group Size</asp:TableCell>
+            <asp:TableCell>Module Code</asp:TableCell>
+            <asp:TableCell>Area</asp:TableCell>
+            <asp:TableCell>Building</asp:TableCell>
+            <asp:TableCell>No. of Rooms</asp:TableCell>
+            <asp:TableCell>Room(s) Specified</asp:TableCell>
+            <asp:TableCell>Priority</asp:TableCell>
+            <asp:TableCell>Status</asp:TableCell>
+            <asp:TableCell>Special Requirements</asp:TableCell>
+            <asp:TableCell>Edit</asp:TableCell>
+            <asp:TableCell>Delete</asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+</section>
+</asp:Content>
---
+++ WebApplication4-0/ViewRequest.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WebApplication4_0
+{
+    public partial class ViewRequest : System.Web.UI.Page
+    {
+        static SqlConnection conn;
+        DataTable dt = new DataTable();
+        public String data = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+            conn.Open();
+
+            string vreqQuery = "SELECT Request_ID, Module_Code, Day, Start_Time, End_Time, Semester, Year, Round, Priority, Number_Rooms, Number_Students FROM Requests";
+            SqlCommand comm = new SqlCommand(vreqQuery, conn);
+
+            SqlDataAdapter da = new SqlDataAdapter(comm);
+            da.Fill(dt);
+            System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, object> row;
+            foreach (DataRow dr in dt.Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+            data = serializer.Serialize(rows);
+        
+        }
+
+    }
+}
