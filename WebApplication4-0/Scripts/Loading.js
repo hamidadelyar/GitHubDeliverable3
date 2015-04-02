0576faa4094c0d0b28ad5756610ebfa3bf1d13cf
awjh <a.w.j.hurt-13@student.lboro.ac.uk>
2015-04-02 09:32:40 +0100

    Split timetable JS to external files

---
+++ WebApplication4-0/Scripts/Loading.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+    hideLoader();
+});
+function showLoader()
+{
+    $('.greyOut').show();
+}
+function hideLoader()
+{
+    $('.greyOut').hide();
+}
+function shrink() {
+    $('.greyOut img').animate({
+        height: '5px',
+        width: '5px',
+        padding: '32.5px'
+    },
+    {
+        duration: 1500,
+        easing: 'swing',
+        complete: function () {
+            if (showing) {
+                grow()
+            }
+            else {
+                finAnim()
+            }
+        }
+    });
+}
+function grow() {
+    $('.greyOut img').animate({
+        height: '70px',
+        width: '70px',
+        padding: '0px'
+    },
+    {
+        duration: 1500,
+        easing: 'swing',
+        complete: function () {
+            if (showing) {
+                shrink()
+            }
+            else {
+                finAnim()
+            }
+        }
+    });
+}
+function finAnim() {
+    $('.greyOut img').css('height', '70px');
+    $('.greyOut img').css('width', '70px');
+    $('.greyOut img').css('padding', '0px');
+}
