63fdead1215c2efab909ab5a7d310fa6ae651d78
awjh <a.w.j.hurt-13@student.lboro.ac.uk>
2015-05-05 20:14:06 +0100

    Added add user and forgot password pages

---
+++ WebApplication4-0/AdminFolder/AddUser.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Add User" Language="C#" MasterPageFile="~/AdminFolder/AdminSite.master" AutoEventWireup="true" CodeBehind="AddUser.aspx.cs" Inherits="WebApplication4_0.AdminFolder.AddUser" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ WebApplication4-0/AdminFolder/AddUser.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4_0.AdminFolder
+{
+    public partial class AddUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["LoggedIn"] != null)
+            {
+                if(((string) Session["username"]) != "admin")
+                {
+                    System.Diagnostics.Debug.WriteLine(Session["username"]);
+                    Response.Write("<script>alert('You do not have permission to view this page.');</script>");
+                    Response.Redirect("../Timetable.aspx");
+                }
+            }
+        }
+    }
+}
