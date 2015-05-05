344800f69ab28a93d1275fc2258bc01880e52ce3
awjh <a.w.j.hurt-13@student.lboro.ac.uk>
2015-05-05 15:08:49 +0100

    Added forgot password

---
+++ WebApplication4-0/ForgotPassword.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4_0
+{
+    public partial class ForgotPassword : System.Web.UI.Page
+    {
+        public string userDets = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            userDets = SQLSelect.Select("Users", "Username, Email", "1=1", "");
+        }
+    }
+}
