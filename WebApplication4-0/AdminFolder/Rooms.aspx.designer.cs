dd70a871fde5eefab15d928c505d38ccbdb9f14c
coje4 <j.everett-13@student.lboro.ac.uk>
2015-05-10 23:07:15 +0100

    Room input fix

--- WebApplication4-0/AdminFolder/Rooms.aspx.designer.cs
+++ WebApplication4-0/AdminFolder/Rooms.aspx.designer.cs
@@ -40,13 +40,22 @@ namespace WebApplication4_0.AdminFolder {
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource5;
         
         /// <summary>
-        /// TextBox1 control.
+        /// SqlDataSource7 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource7;
+        
+        /// <summary>
+        /// DropDownList2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
         
         /// <summary>
         /// TextBox2 control.
@@ -67,13 +76,13 @@ namespace WebApplication4_0.AdminFolder {
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
         
         /// <summary>
-        /// TextBox6 control.
+        /// DropDownList3 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
         
         /// <summary>
         /// Button1 control.
